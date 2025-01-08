1fd696bab9403c365df404fcf4076380d3f31762
Prince Vaviya <1princevaviya@gmail.com>
2025-01-08 21:49:51 +0530

    DataTypes

---
+++ DataTypes.java
@@ -0,0 +1,28 @@
+class DataTypes{
+    public static void main(String args[]){
+        //integer
+        byte b = 100;
+        short s = 1000;
+        int i = 10000;
+        long l = 100000;
+
+        //character
+        char c = 'C';
+
+        //float
+        float f = 7.7e+7f;
+        double d = 7.7e+77;
+
+        //bool
+        boolean b00l = true;
+
+        System.out.println("The byte : " + b);
+        System.out.println("The short : " + s);
+        System.out.println("The int : " + i);
+        System.out.println("The long : " + l);
+        System.out.println("The char : " + c);
+        System.out.println("The float : " + f);
+        System.out.println("The double : " + d);
+        System.out.println("The boolean : " + b00l);
+    }
+}
