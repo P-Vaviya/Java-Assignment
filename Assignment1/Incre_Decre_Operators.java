01084dc7d28736704a980974d04f88865c3ed0a7
Prince Vaviya <1princevaviya@gmail.com>
2025-01-09 08:50:02 +0530

    Assignment1

--- Assignment1/Incre_Decre_Operators.java
+++ Assignment1/Incre_Decre_Operators.java
@@ -9,7 +9,19 @@ class Incre_Decre_Operators {
         int c = 30;
         int d = ++c;
 
-        System.out.println(b);
-        System.out.println(d);
+        System.out.println("post increment for 20 : " + b);
+        System.out.println("pre increment for 30 : " + d);
+
+//------------------------------------------------------------//----------------------------------------------------//
+
+        //post-decrement operator
+        int num1 = 20;
+        int num2 = num1--;
+        //pre-decrement operator
+        int num3 = 30;
+        int num4 = --num3;
+
+        System.out.println("post decrement for 20 : " + num2);
+        System.out.println("pre decrement for 30 : " + num4);
     }
 }
